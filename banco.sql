d32060af64b571b817d1f3aa78280496325d029e
detriste <detriste2018@gmail.com>
2026-02-18 14:45:32 -0300

    pronto banco funcional

--- banco.sql
+++ banco.sql
@@ -1,62 +1,99 @@
--- Criar banco
-CREATE SCHEMA IF NOT EXISTS `tcc`
-DEFAULT CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci;
+DROP DATABASE IF EXISTS tcc;
+-- MySQL Workbench Forward Engineering
 
-USE `tcc`;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Tabela usuarios
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL,
-  `sobrenome` VARCHAR(100) NOT NULL,
-  `email` VARCHAR(150) NOT NULL,
-  `cpf` VARCHAR(14) NOT NULL,
-  `senha` VARCHAR(255) NOT NULL,
-  `saldo` DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-  PRIMARY KEY (`id`),
-  UNIQUE (`email`),
-  UNIQUE (`cpf`)
-) ENGINE=InnoDB;
+CREATE SCHEMA IF NOT EXISTS tcc DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE tcc ;
 
 -- -----------------------------------------------------
--- Tabela viagens
+-- Table tcc.viagens
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `viagens` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `origem` VARCHAR(100) NOT NULL,
-  `destino` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+DROP TABLE IF EXISTS tcc.viagens ;
+
+CREATE TABLE IF NOT EXISTS tcc.viagens (
+  id INT NOT NULL AUTO_INCREMENT,
+  origem VARCHAR(45) NOT NULL,
+  destino VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id))
+ENGINE = InnoDB;
+
 
 -- -----------------------------------------------------
--- Tabela tickets
+-- Table tcc.usuarios
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tickets` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `duracao` TIME NOT NULL,
-  `quantidade` INT NOT NULL,
-  `valor` DECIMAL(10,2) NOT NULL,
-  `ativacao` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` ENUM('ativo', 'usado') NOT NULL DEFAULT 'ativo',
-  `usuario_id` INT NOT NULL,
-  `viagem_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`usuario_id`) REFERENCES `usuarios`(`id`) ON DELETE CASCADE,
-  FOREIGN KEY (`viagem_id`) REFERENCES `viagens`(`id`) ON DELETE CASCADE
-) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS tcc.usuarios (
+  id INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(100) NOT NULL,
+  sobrenome VARCHAR(100) NOT NULL,
+  email VARCHAR(150) NOT NULL,
+  cpf VARCHAR(14) NOT NULL,
+  senha VARCHAR(255) NOT NULL,
+  saldo DECIMAL(10,3) NOT NULL DEFAULT 0,
+  PRIMARY KEY (id),
+  UNIQUE INDEX email (email ASC) VISIBLE,
+  UNIQUE INDEX cpf (cpf ASC) VISIBLE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
 
 -- -----------------------------------------------------
--- Tabela pagamentos
+-- Table tcc.tickets
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pagamentos` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `valor` DECIMAL(10,2) NOT NULL,
-  `forma_pagamento` ENUM('Credito', 'Debito', 'Pix') NOT NULL,
-  `data_pagamento` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `usuario_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`usuario_id`) REFERENCES `usuarios`(`id`) ON DELETE CASCADE
-) ENGINE=InnoDB;
+DROP TABLE IF EXISTS tcc.tickets ;
+
+CREATE TABLE IF NOT EXISTS tcc.tickets (
+  id INT NOT NULL AUTO_INCREMENT,
+  duração TIME NOT NULL,
+  quantidade INT NOT NULL,
+  valor DECIMAL(10,3) NOT NULL,
+  ativação TIMESTAMP NOT NULL,
+  status ENUM('ativo', 'usado') NOT NULL,
+  viagens_id INT NOT NULL,
+  usuarios_id INT NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX cpf (duração ASC) VISIBLE,
+  INDEX fk_tickets_viagens1_idx (viagens_id ASC) VISIBLE,
+  INDEX fk_tickets_usuarios1_idx (usuarios_id ASC) VISIBLE,
+  CONSTRAINT fk_tickets_viagens1
+    FOREIGN KEY (viagens_id)
+    REFERENCES tcc.viagens (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_tickets_usuarios1
+    FOREIGN KEY (usuarios_id)
+    REFERENCES tcc.usuarios (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table tcc.pagamentos
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS tcc.pagamentos ;
+
+CREATE TABLE IF NOT EXISTS tcc.pagamentos (
+  id INT NOT NULL AUTO_INCREMENT,
+  valor DECIMAL(10,3) NOT NULL,
+  formas_pagamento ENUM('Crédito', 'Debito', 'Pix') NOT NULL,
+  data TIMESTAMP NOT NULL,
+  tickets_id INT NOT NULL,
+  PRIMARY KEY (id),
+  INDEX fk_pagamentos_tickets_idx (tickets_id ASC) VISIBLE,
+  CONSTRAINT fk_pagamentos_tickets
+    FOREIGN KEY (tickets_id)
+    REFERENCES tcc.tickets (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
